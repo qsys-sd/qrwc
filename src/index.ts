a4e9a626431b7db7d72f25f3c577b39a78391388
Devin.Kapla <Devin.Kapla@qsc.com>
2023-09-07 11:46:19 -0600

    QSYSDEV-29310 Create message handlers

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,27 @@
 import { IChangeGroup, IControl, IQrccOptions } from "./index.interface"
-import { v4 as uuidv4 } from 'uuid';
-import { createJSONRPCMessage, qrcMethods } from "./index.util";
+import { v4 as uuidv4 } from 'uuid'
+import { createJSONRPCMessage, qrcMethods } from "./index.util"
+import { EventEmitter } from 'events'
 
-export default class Qrcc {
+export default class Qrcc extends EventEmitter {
   private url: string = ""
   private websocket: WebSocket | null = null
   private socketPollId: number = 1
   private componentsGetId: string = ""
   private controlGetIds: string[] = []
   readonly pollInterval: number = 3000
+  readonly qrccEvents: { [key: string]: string } = {
+    controlUpdated: "controlUpdated",
+    constrolAdded: "controlAdded",
+    controlsRecieved: "controlsReceived",
+  }
   changeGroups: IChangeGroup[] = []
   componentList: string[] = []
   controls: IControl[]
   autoStart: boolean = false
 
   constructor(options: IQrccOptions) {
+    super()
     this.url = options.url
     this.pollInterval = options.pollInterval
     this.autoStart = options.autoStart
@@ -65,16 +72,94 @@ export default class Qrcc {
     this.componentsGetId = ""
   }
 
-  private handleControlGetResponse(data: any) {
-    if (data.result.Controls) {
-      this.controls = [...this.controls, ...data.result.Controls]
-    } else if (data.result) {
-      this.controls = [...this.controls, data.result]
+  // a function that takes a results object and returns an array of controls
+  private getControlsFromMessage = (results: any) => {
+    // check if results is an array or a single object and convert to array if needed
+    if(results.Controls) {
+      return results.Controls
+    } else if (results) {
+      return [results]
     } else {
-      console.error("No controls found in response.")
+      // return empty array if results is null
+      return []
+    }
+  }
+
+  // a function that take an array of controls and returns an array of controls that do not exist in this.controls
+  private filterExistingControls(controls: any[]) {
+    return controls.filter((control: any) => !this.controls.find((c: any) => c.Name === control.Name))
+  }
+
+  // a function that takes an array of controls and returns an array of controls that do exist in this.controls
+  private filterNonExistingControls(controls: any[]) {
+    return controls.filter((control: any) => this.controls.find((c: any) => c.Name === control.Name))
+  }
+
+  // a function that takes a control and replaces the existing control with the same name
+  private replaceExistingControl(control: any) {
+    // find existing control
+    const existingControl = this.controls.find((c: any) => c.Name === control.Name)
+    // check if existing control exists
+    if (existingControl) {
+      // remove existing control from this.controls
+      this.controls = this.controls.filter((c: any) => c.Name !== control.Name)
+      // add updated control to this.controls
+      this.controls = [...this.controls, control]
+      // emit event
+      this.handleEvent(this.qrccEvents.controlUpdated, control, this.controls)
+    } else {
+      // emit warning
+      console.warn("Existing control not found.")
+    }
+  }
+
+  // a function that takes a new control and adds it to this.controls and emits an event
+  private addNewControl(control: any) {
+    // add new control to this.controls
+    this.controls = [...this.controls, control]
+    // emit event
+    this.handleEvent(this.qrccEvents.constrolAdded, control, this.controls)
+  } 
+
+  // a function that takes data from a controlGet response and adds new controls to this.controls and replaces existing controls in this.controls
+  private handleControlGetResponse(data: any) {
+    // check if data.result is an array or a single object and convert to array if needed
+    const controls = this.getControlsFromMessage(data.result)
+    // filter out controls that already exist in this.controls
+    const newControls = this.filterExistingControls(controls)
+    // filter out controls that do not exist in this.controls
+    const existingControls = this.filterNonExistingControls(controls)
+
+    //check if new controls array is not empty
+    if (newControls.length > 0) {
+      // add new controls to this.controls
+      newControls.forEach((control: any) => {
+        this.addNewControl(control)
+      })
+    }
+
+    // check if existing controls array is not empty
+    if (existingControls.length > 0) {
+      // replace existing controls in this.controls
+      existingControls.forEach((control: any) => {
+        this.replaceExistingControl(control)
+      })
+    }
+
+    //check if both new and existing controls arrays are empty
+    if (newControls.length === 0 && existingControls.length === 0) {
+      // emit warning
+      console.warn("No new or existing controls found in response.")
     }
     
+    // remove id from controlGetIds
     this.controlGetIds = this.controlGetIds.filter((id: string) => id !== data.id)
+
+    // check if controlGetIds is empty
+    if (this.controlGetIds.length === 0) {
+      // emit event
+      this.handleEvent(this.qrccEvents.controlsRecieved, this.controls)
+    }
   }
 
   private onError(event: Event): void {
@@ -114,6 +199,10 @@ export default class Qrcc {
       createJSONRPCMessage(qrcMethods.changeGroup.poll, {Id: changeGroupId}, this.socketPollId)
     )
     this.socketPollId++
+  }
+
+  private handleEvent(eventName: string, ...args: any[]) {
+    this.emit(eventName, ...args);
   }
 
   // public methods that can call private handlers
