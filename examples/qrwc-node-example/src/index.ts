ff7595578fa065a858eda6ab5990a8d14f8ea5ef
Caroline Gingles <caroline.gingles@qsc.com>
2025-06-04 14:23:11 -0600

    QSYSDEV-47902: make the examples work better

--- examples/qrwc-node-example/src/index.ts
+++ examples/qrwc-node-example/src/index.ts
@@ -1,39 +1,39 @@
-import { Qrwc, IControlState } from "@q-sys/qrwc";
-import WebSocket from "ws";
+import { Qrwc, IControlState } from '@q-sys/qrwc'
+import WebSocket from 'ws'
+import 'dotenv/config'
 
 const setupConnection = async () => {
-  const youCoreIP = "10.126.13.6";
+  const coreIP = process.env.CORE_IP_ADDRESS ?? ''
 
-  const socket = new WebSocket(`ws://${youCoreIP}/qrc-public-api/v0`);
+  const socket = new WebSocket(`ws://${coreIP}/qrc-public-api/v0`)
 
   const qrwc = await Qrwc.createQrwc<{
-    Gain: "gain" | "mute";
-    Gain_1: "gain";
-    Gain_2: "gain";
+    Gain: 'gain' | 'mute'
+    Gain_1: 'gain'
+    Gain_2: 'gain'
   }>({
     socket,
-    pollingInterval: 34,
-  });
+    pollingInterval: 34
+  })
 
-  const gain0 = qrwc.components.Gain.controls.gain;
-  const gain1 = qrwc.components.Gain_1.controls.gain;
-  const gain2 = qrwc.components.Gain_2.controls.gain;
-  // const gain3 = qrwc.components.Gain_3.controls.gain;
+  const gain0 = qrwc.components.Gain.controls.gain
+  const gain1 = qrwc.components.Gain_1.controls.gain
+  const gain2 = qrwc.components.Gain_2.controls.gain
 
-  const avg = async (state: IControlState) => {
+  const avg = async (_state: IControlState) => {
     await gain0.update(
       ((gain1.state.Value ?? 0) + (gain2.state.Value ?? 0)) / 2
-    );
-  };
+    )
+  }
 
-  gain1.on("update", avg);
+  gain1.on('update', avg)
 
-  gain2.on("update", avg);
+  gain2.on('update', avg)
 
-  qrwc.on("disconnected", () => {
+  qrwc.on('disconnected', () => {
     setTimeout(() => {
-      setupConnection();
-    }, 1000);
-  });
-};
-setupConnection();
+      setupConnection()
+    }, 1000)
+  })
+}
+setupConnection()
