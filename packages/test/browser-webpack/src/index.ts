27ba79fd39258c74934dd30989cd2b70dd1750c6
Caroline Gingles <caroline.gingles@qsc.com>
2025-10-20 16:59:52 -0600

    fix test typing?

--- packages/test/browser-webpack/src/index.ts
+++ packages/test/browser-webpack/src/index.ts
@@ -4,7 +4,7 @@ const testElement = document.getElementById('test')
 const test = async () => {
   const socket = new WebSocket('ws://localhost:3102')
   const qrwc = await Qrwc.createQrwc({ socket })
-  if (testElement) {
+  if (qrwc.components.TestComponent1 && testElement) {
     testElement.textContent = qrwc.components.TestComponent1.name
     testElement.click()
   }
--- packages/test/node-esm/src/index.ts
+++ packages/test/node-esm/src/index.ts
@@ -4,7 +4,10 @@ import { WebSocket } from 'ws'
 const test = async () => {
   const socket = new WebSocket('ws://localhost:3104')
   const qrwc = await Qrwc.createQrwc({ socket })
-  if (qrwc.components.TestComponent1.name !== 'TestComponent1') {
+  if (
+    qrwc.components.TestComponent1 &&
+    qrwc.components.TestComponent1.name !== 'TestComponent1'
+  ) {
     qrwc.close()
     console.warn(
       ' \x1b[30m\x1b[41m FAIL \x1b[0m - \x1b[30m\x1b[47m node-esm \x1b[0m'
